71a09ca17761d56e0789c13191009413c0cb44f8
Tsilos Kosbab <38735446+Tsilos1@users.noreply.github.com>
2022-10-27 21:14:02 -0500

    final commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,41 @@
+                
+import { useState } from 'react'
+
+function SearchBar(props){
+    let [searchTerm, setSearchTerm] = useState('')
+
+    return (
+        <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+
+            <input type="text" placeholder="Enter a search term here"
+            onChange={(e) => setSearchTerm(e.target.value)} />
+
+            <input type="submit" />
+
+        </form>
+    )
+}
+
+
+        export default SearchBar
+
+
+//to have searchbar search without the submit button and instead with each letter
+// import { useState } from 'react'
+
+// function SearchBar(props){
+//     // We can comment out our searchTerm state variable as we are not using it!
+//     // let [searchTerm, setSearchTerm] = useState('')
+
+//     return (
+//             <form>
+//                 <input type="text" placeholder="Search Here"
+//                     onChange={
+//                         (e) => props.handleSearch(e, e.target.value)
+//                     } />
+//                 <input type="submit" />
+//             </form>
+//     )
+// }
+
+// export default SearchBar
